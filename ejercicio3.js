49010a42d70dda95fe6f338cd2836181dcf42843
jhoan <jhoan17sebastia@gmail.com>
2024-09-27 21:54:44 -0500

    Agrege solución para el ejercicio 3

---
+++ ejercicio3.js
@@ -0,0 +1,7 @@
+// Ejercicio 3: Declarar y asignar valor a una variable llamada numero1 y numero2.
+// Imprimir la suma en la consola.
+let  numero1 = 2345;
+let  numero2 = 34546;
+let  suma = numero1 + numero2;
+console.log(" La suma entre " + numero1 + " y " + numero2 + " es " + suma); // la suma es 36891
+
