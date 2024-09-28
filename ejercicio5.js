416780473b44c726673270b381df6f55a2d8fb8d
jhoan <jhoan17sebastia@gmail.com>
2024-09-27 22:03:45 -0500

    agrege la solucion el problema 5

---
+++ ejercicio5.js
@@ -0,0 +1,9 @@
+// Ejercicio 5: Calcular el índice de masa corporal (IMC).
+// IMC = peso / altura**2. El resultado se imprimirá en la consola.
+
+let peso = 74;
+let altura = 1.72; 
+
+let imc = peso / (altura ** 2);
+
+console.log(`Tu IMC es ${imc}`);
