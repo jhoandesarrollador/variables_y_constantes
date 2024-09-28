3afd3026414871b42fb6deddd32730a52e80036f
jhoan <jhoan17sebastia@gmail.com>
2024-09-27 21:15:17 -0500

    Agrege solución para el ejercicio 2

---
+++ ejercicio2.js
@@ -0,0 +1,4 @@
+// Ejercicio 2: Defina una variable llamada nombre y asigne su nombre.
+// Debe imprimir en la consola "Hola, jhoan !" usando template literals.
+let nombre = "jhoan Sebastian Sanchez"; 
+console.log(`Hola, ${nombre}!`);
